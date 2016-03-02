0277154bf56f0e9c0d543779e7b26de17ac5d73d
Darío Javier Cravero <dario@uxtemple.com>
2016-03-02 20:53:03 +0000

    fix: convert exp from s to ms to match Date.now()

--- __tests__/get-expires-test.js
+++ __tests__/get-expires-test.js
@@ -2,7 +2,8 @@ import { stub } from 'sinon';
 import getExpires from '../get-expires';
 import test from 'tape';
 
-const EXP = 'exp';
+const EXP = 2;
+const EXP_MS = EXP * 1000;
 
 const PAYLOAD_WITH_EXP = 'payload-with-exp';
 const PAYLOAD_WITH_INVALID_JSON = 'payload-with-invalid-json';
@@ -13,14 +14,14 @@ const TOKEN_WITH_INVALID_JSON = `header.${PAYLOAD_WITH_INVALID_JSON}.signature`;
 const TOKEN_WITHOUT_EXP = `header.${PAYLOAD_WITHOUT_EXP}.signature`;
 
 const atob = stub();
-atob.withArgs(PAYLOAD_WITH_EXP).returns(`{"exp": "${EXP}"}`);
+atob.withArgs(PAYLOAD_WITH_EXP).returns(`{"exp": ${EXP}}`);
 atob.withArgs(PAYLOAD_WITH_INVALID_JSON).returns(`"`);
 atob.withArgs(PAYLOAD_WITHOUT_EXP).returns(`{}`);
 
 test('#getExpires', t => {
   global.atob = atob;
 
-  t.equals(getExpires(TOKEN_WITH_EXP), EXP, 'returns expiration when it exists');
+  t.equals(getExpires(TOKEN_WITH_EXP), EXP_MS, 'returns expiration when it exists');
   t.notOk(getExpires(TOKEN_WITHOUT_EXP), `returns false when the token doesn't expire`);
 
   t.throws(() => getExpires(TOKEN_WITH_INVALID_JSON), 'throws when JSON in payload is invalid');
--- get-expires.js
+++ get-expires.js
@@ -1,3 +1,3 @@
 export default function getExpires(token) {
-  return JSON.parse(atob(token.split('.')[1])).exp || false;
+  return JSON.parse(atob(token.split('.')[1])).exp * 1000 || false;
 }
